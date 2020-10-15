7f078954dc8d9c3a7f4b98f775f01d8bc9686de4
Ruby Werman <rubywerman@berkeley.edu>
2020-10-14 17:06:08 -0700

    change string formatting

--- ruby-scripts/getPeople.py
+++ ruby-scripts/getPeople.py
@@ -1,14 +1,11 @@
 import requests
 import json
+import os
 
+PYBOSSA_API_KEY = os.getenv('PYBOSSA_API_KEY')
 headers = {
   'Content-Type': 'application/json',
-  'X-API-KEY': '',
 }
-api_key = ''
-project_names = ["Covid2_FormTriage", "Covid2_SemanticsTriage"]
-project_ids = ['253', '254']
-user_ids = {}
 
 
 def get_ids(project_ids):
@@ -17,17 +14,19 @@ def get_ids(project_ids):
     Output: Dictionary of (key) user_ids and (value) the amount of tasks
     they've completed for the given projects
     """
+    user_ids = {}
     for project_id in project_ids:
-        r = requests.get('https://pe.goodlylabs.org/api/taskrun?api_key='
-                         + api_key + '&project_id=' + project_id +
-                         '&orderby=id&limit=100', headers=headers)
+        r = requests.get('https://pe.goodlylabs.org/api/taskrun?api_key={}'
+                         '&project_id={}&orderby=id&limit=100'
+                         .format(PYBOSSA_API_KEY, project_id),
+                         headers=headers)
         task_runs = json.loads(r.text)
         # For each task, either add the user id or incremement the id's count
         for task in task_runs:
-            if str(task["user_id"]) in user_ids:
-                user_ids[str(task["user_id"])] += 1
+            if str(task['user_id']) in user_ids:
+                user_ids[str(task['user_id'])] += 1
             else:
-                user_ids[str(task["user_id"])] = 1
+                user_ids[str(task['user_id'])] = 1
     return user_ids
 
 
@@ -38,16 +37,19 @@ def get_names(user_ids):
     """
     names = {}
     for user in user_ids:
-        r = requests.get('https://pe.goodlylabs.org/api/user/'
-                         + user + '?api_key=' + api_key, headers=headers)
+        r = requests.get('https://pe.goodlylabs.org'
+                         '/api/user/{}?api_key={}&limit=100'
+                         .format(user, PYBOSSA_API_KEY), headers=headers)
         user_info = json.loads(r.text)
         names[user_info['fullname']] = user_ids[user]
     return names
 
 
-if __name__ == "__main__":
-    # Write dictionary to CSV file
-    names_dict = get_names(get_ids(project_ids))
-    with open('people.csv', 'w') as f:
+if __name__ == '__main__':
+    project_names = ['Covid2_FormTriage', 'Covid2_SemanticsTriage']
+    project_ids = ['253', '254']
+    user_ids = get_ids(project_ids)
+    names_dict = get_names(user_ids)
+    with open('hi.csv', 'w') as f:
         for key in names_dict.keys():
             f.write("%s, %s\n" % (key, names_dict[key]))
