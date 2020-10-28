6f341b992051a3360c5f6caa6699fc6481f33258
Ruby Werman <rubywerman@berkeley.edu>
2020-10-28 14:26:42 -0700

    remove create_tables duplicate function

--- event_model/insert_users.py
+++ event_model/insert_users.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from event_model.base import Base
 import pandas as pd
+from event_model.create_tables import create_gamify_tables
 
 
 # Tables in Gamify Model
@@ -16,24 +17,10 @@ from event_model.email_candidate import EmailCandidate
 from event_model.email_purpose import EmailPurpose
 
 
-def create_gamify_tables(echo=False):
-    engine = create_engine(
-        "postgresql://pe_dashboard:test-only-yVu8W5azUtZ8RPSWX42o@"
-        "localhost:5432/pe_dashboard",
-        echo=echo,
-    )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    Base.metadata.create_all(engine)
-
-    return session, engine
-
-
 if __name__ == "__main__":
     csv_file_path = "/Users/rubywerman/desktop/pe-gamification/"\
                     "ruby-scripts/hi.csv"
-    session, engine = create_gamify_tables(False)
+    session, engine = create_gamify_tables(echo=False, returnEngine=True)
     # Read in CSV file
     with open(csv_file_path, 'r') as file:
         data_df = pd.read_csv(file)
