fea1a995dd4ad4c2de5b930ae6a1b4d8e1173aac
Ruby Werman <rubywerman@berkeley.edu>
2020-10-30 15:07:40 -0700

    fix flake8

--- pybossa_api/get_people.py
+++ pybossa_api/get_people.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import os
-import csv
 
 PYBOSSA_API_KEY = os.getenv('PYBOSSA_API_KEY')
 headers = {
@@ -45,35 +44,8 @@ def get_names(user_ids):
         names[user_info['fullname']] = user_ids[user]
     return names
 
-def fill_user(user_ids):
-    """
-    Input: user_ids dictionary (user ids: task values)
-    Output: csv file with user id, name, email
-    """
-    emails = {}
-    for user in user_ids:
-        r = requests.get('https://pe.goodlylabs.org'
-                         '/api/user/{}?api_key={}&limit=100'
-                         .format(user, PYBOSSA_API_KEY), headers=headers)
-        user_info = json.loads(r.text)
-        emails[user] = [user_info['fullname'], user_info['email_addr']]
-    with open('user.csv', 'w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["id", "name", "email"])
-        for i in emails:
-            writer.writerow([i, emails[i][0], emails[i][1]])
-
-
 
 if __name__ == '__main__':
     project_names = ['Covid2_FormTriage', 'Covid2_SemanticsTriage']
     project_ids = ['253', '254']
     user_ids = get_ids(project_ids)
-
-    emails = fill_user(user_ids)
-    print(emails)
-    #names_dict = get_names(user_ids)
-"""
-    with open('hi.csv', 'w') as f:
-        for key in names_dict.keys():
-            f.write("%s, %s\n" % (key, names_dict[key]))"""
