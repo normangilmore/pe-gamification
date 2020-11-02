2e32e637e9b0143f9f9369110213167e8fdd4e08
Jeremy Berkowitz <jerberk@berkeley.edu>
2020-11-02 15:38:13 -0800

    jeremy taskrun
    jeremy_getpeople
    
    j_get_people

--- pybossa_api/get_people.py
+++ pybossa_api/get_people.py
@@ -76,7 +76,43 @@ def get_projectIDs(project_names):
     return project_ids
 
 
+
+
+
+def fill_taskrun(user_ids, project_ids):
+    '''
+    Input: taskrun_dict dictionary (user id: task values)
+    Output: csv file with taskrun id, timestamp, project id, task id,
+    and user id
+    '''
+    task_dict = {}
+    for project_id in project_ids:
+        r = requests.get('https://pe.goodlylabs.org/api/taskrun?api_key={}'
+                         '&project_id={}&orderby=id&limit=100'
+                         .format(PYBOSSA_API_KEY, project_id),
+                         headers=headers)
+        task_runs = json.loads(r.text)
+        index = 0
+        for task_run in task_runs:
+            index += 1
+            task_dict[index] = [task_run['id'], task_run['created'], task_run['project_id'], task_run['task_id'],
+            task_run['user_id'], task_run['finish_time'], task_run['info']]
+    with open('task_run.csv', 'w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["id", "created", "project_id", "task_id", "user_id", "finish_time", 'info'])
+        for i in task_dict:
+            writer.writerow([task_dict[i][0], task_dict[i][1], task_dict[i][2], task_dict[i][3], task_dict[i][4], task_dict[i][5]])
+
+
+
+
+
 if __name__ == '__main__':
     project_names = ['Covid2_FormTriage', 'Covid2_SemanticsTriage']
     project_ids = ['253', '254']
+    
     user_ids = get_ids(project_ids)
+    #emails = fill_taskrun(user_ids, project_ids)
+    #emails = fill_user(user_ids)
+    #print(emails)
+    #names_dict = get_names(user_ids)
