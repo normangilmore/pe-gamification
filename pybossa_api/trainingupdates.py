5ca662ddb1bc0396815a13077bf16e60c623df15
Ruby Werman <rubywerman@berkeley.edu>
2020-11-02 12:36:19 -0800

    add comments

--- pybossa_api/trainingupdates.py
+++ pybossa_api/trainingupdates.py
@@ -36,12 +36,20 @@ def generateCSV(user_ids, filename='user', write=True):
 
 
 if __name__ == '__main__':
+    # Write the project names you want to retrieve task data on
+    # Right now, these are the training tasks
     project_names = ['Covid_SourceRelevancev1', 'Covid_Semantics1.0',
                      "Covid_Reasoningv1", "Covid_Probabilityv1",
                      "Covid_Languagev1.1", "Covid_Holisiticv1.2",
                      "Covid_Form1.0", "Covid_Evidencev1",
                      "Covid_ArgumentRelevancev1.2"]
+    # Call project_ids to retrieve the project IDs of each project
     project_ids = get_projectIDs(project_names)
     for name in project_ids:
-        user_ids = get_ids([project_ids[name]], ['2020-09', '2020-10'])
+        # Call get_ids to retrieve a list of user IDs and how many tasks
+        # they've completed for a given project
+        # Here, we specify the months we want to retrieve tasks from
+        user_ids = get_ids([project_ids[name]], months=['2020-09', '2020-10'])
+        # Finally, we generate the CSV for each project, giving us the name, 
+        # email, and number of tasks each person has done for the given months
         generateCSV(user_ids, name)
