ef144a6807da70e1706223bfdc9588ba1c405119
Ruby Werman <44390541+rubywerman@users.noreply.github.com>
2020-11-04 11:53:08 -0800

    remove jeremys function

--- pybossa_api/get_people.py
+++ pybossa_api/get_people.py
@@ -32,8 +32,8 @@ def get_ids(project_ids, months=None):
     else:
         for project_id in project_ids:
             r = requests.get('https://pe.goodlylabs.org/api/taskrun?api_key={}'
-                             '&project_id={}&created={}&all=1&orderby=id&limit=100'
-                             .format(PYBOSSA_API_KEY, project_id, m),
+                             '&project_id={}&all=1&orderby=id&limit=100'
+                             .format(PYBOSSA_API_KEY, project_id),
                              headers=headers)
             task_runs = json.loads(r.text)
             # For each task, either add the user id or incremement the id's count
@@ -75,38 +75,7 @@ def get_projectIDs(project_names):
         project_ids[project_name] = (project_info[0]['id'])
     return project_ids
 
-
-
-
-
-def fill_taskrun(user_ids, project_ids):
-    '''
-    Input: taskrun_dict dictionary (user id: task values)
-    Output: csv file with taskrun id, timestamp, project id, task id,
-    and user id
-    '''
-    task_dict = {}
-    for project_id in project_ids:
-        r = requests.get('https://pe.goodlylabs.org/api/taskrun?api_key={}'
-                         '&project_id={}&orderby=id&limit=100'
-                         .format(PYBOSSA_API_KEY, project_id),
-                         headers=headers)
-        task_runs = json.loads(r.text)
-        index = 0
-        for task_run in task_runs:
-            index += 1
-            task_dict[index] = [task_run['id'], task_run['created'], task_run['project_id'], task_run['task_id'],
-            task_run['user_id'], task_run['finish_time'], task_run['info']]
-    with open('task_run.csv', 'w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["id", "created", "project_id", "task_id", "user_id", "finish_time", 'info'])
-        for i in task_dict:
-            writer.writerow([task_dict[i][0], task_dict[i][1], task_dict[i][2], task_dict[i][3], task_dict[i][4], task_dict[i][5]])
-
-
-
-
-
+  
 if __name__ == '__main__':
     project_names = ['Covid2_FormTriage', 'Covid2_SemanticsTriage']
     project_ids = ['253', '254']
