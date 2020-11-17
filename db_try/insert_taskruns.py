3acaca9ce863638e27a5027e821afbb07131792b
Denny <dennydang@berkeley.edu>
2020-11-17 14:53:31 -0800

    add query and fix insertion

--- db_try/insert_taskruns.py
+++ db_try/insert_taskruns.py
@@ -1,17 +1,23 @@
-from sqlalchemy import create_engine, MetaData
-from taskrun import taskruns
+from sqlalchemy import create_engine, MetaData, select
+from taskrun import taskruns, users
 import csv
 
+engine = create_engine("postgresql://pe_dashboard:test-only-yVu8W5azUtZ8RPSWX42o@localhost:5432/pe_dashboard", echo=True)
+meta = MetaData()
+conn = engine.connect()
+
 rows = []
+get_user_ids = select([users.c.id])
+user_ids = [uid[0] for uid in conn.execute(get_user_ids)]
+
 with open('tr.csv', newline='') as pscfile:
     reader = csv.DictReader(pscfile)
     for row in reader:
+        if int(row['user_id']) not in user_ids:
+            continue
         r = {'created': row['created'], 'task_id': row['task_id'],
              'user_id': row['user_id'], 'finish_time': row['finish_time'],
              'task_type': row['task_type']}
         rows.append(r)
 
-engine = create_engine("postgresql://pe_dashboard:test-only-yVu8W5azUtZ8RPSWX42o@localhost:5432/pe_dashboard", echo=True)
-meta = MetaData()
-conn = engine.connect()
 conn.execute(taskruns.insert(), rows)
--- db_try/query.py
+++ db_try/query.py
@@ -1,18 +1,36 @@
-from sqlalchemy import create_engine, MetaData
-from taskrun import users
+from sqlalchemy import create_engine, MetaData, func, select
+from taskrun import users, taskruns
+from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine("postgresql://pe_dashboard:test-only-yVu8W5azUtZ8RPSWX42o@localhost:5432/pe_dashboard", echo=True)
 meta = MetaData()
-
-s = users.select()
 conn = engine.connect()
-result = conn.execute(s)
+
+# u = users.select()
+# tr = taskruns.select()
+# user_result = conn.execute(u)
+# taskruns_result = conn.execute(tr)
 
 """
 SELECT name, email
 FROM users
-WHERE user.id == taskruns.user_id
+WHERE user.user_id == taskruns.user_id
 AND taskruns.task_type == 'Form'
 GROUP BY name
 having count(name) > 50
 """
+
+# did not use having clause because none of these results seem to have over 50 task runs
+join_stmt = users.join(taskruns)
+test_query = select([users.c.name, func.count(users.c.name)]).select_from(join_stmt).where(taskruns.c.task_type == "Form").group_by(users.c.id)
+result = conn.execute(test_query).fetchall()
+for r in result:
+    print(r)
+    
+# test_query = session.query(
+#     User).join(
+#         Taskrun, Taskrun.c.user_id == User.c.user_id).filter(
+#             Taskrun.c.task_type == 'Form').group_by(
+#                 User.c.user_id).all()
+# s
